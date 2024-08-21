2e183bae40a1757eca31eee5b5eb6d1c75d56d90
omengoemmanuel <omengomanuel@gmail.com>
2024-08-21 13:43:24 +0300

    api final

--- API/views.py
+++ API/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import productserializer
+from .models import product
+from rest_framework import viewsets
+
 
 # Create your views here.
+class productview(viewsets.ModelViewSet):
+    queryset = product.objects.all()
+    serializer_class = productserializer
