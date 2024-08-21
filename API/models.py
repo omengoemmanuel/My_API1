3484798312cca1ca9372864856d271f07310e11e
omengoemmanuel <omengomanuel@gmail.com>
2024-08-21 12:32:57 +0300

    creating table

--- API/models.py
+++ API/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='products', default='product/product.jpg')
+
+    def __str__(self):
+        return self.name
